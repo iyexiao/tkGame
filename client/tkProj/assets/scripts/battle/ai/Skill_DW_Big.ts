0d2b0993ad2981fa9d0ebd773a0607657c4a4283
YeXiao <iyexiao@163.com>
2019-03-30 21:13:09 +0800

    fix:修改典韦技能

---
+++ client/tkProj/assets/scripts/battle/ai/Skill_DW_Big.ts
@@ -0,0 +1,17 @@
+import SkillAiBase from "./AiBase"
+
+/**
+ * @class Skill_DW_Big
+ * @author YeXiao
+ * @deprecated 典韦大招
+ * @since 2019-3-30 21:12:48
+ */
+export default class Skill_DW_Big extends SkillAiBase{
+    private _skillArr:Array<string> = null;
+    constructor(skillArr?:Array<string>){
+        super("Skill_DW_Big");
+        if (skillArr) {
+            this._skillArr = skillArr;
+        }
+    }
+}
